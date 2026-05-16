e0a0959cc572a38bfbcaad7c98286fa8b323da46
Lukas Zimmermann <lukas.zimmermann@schoggipopcorn.ch>
2026-05-16 23:54:31 +0200

    add statistics

--- src/resource_monitor/resource_monitor/rpi_monitor/rpi_monitor_node.py
+++ src/resource_monitor/resource_monitor/rpi_monitor/rpi_monitor_node.py
@@ -3,7 +3,7 @@ import platform
 from rclpy.node import Node
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
 from resource_monitor.rpi_monitor.rpi_monitor import RpiMonitor, RpiMetric
-from resource_monitor.diagnostic import DiagnosticPublisher, DiagnosticParameter
+from resource_monitor.resource_monitor.status_builder import StatusBuilder, StatusParameter
 import platform
 
 class RpiMonitorNode(Node):
@@ -14,13 +14,13 @@ class RpiMonitorNode(Node):
         self.rpi_monitor = RpiMonitor()
         self.hardware_id = f"cm5_{platform.node()}".replace("-", "_")
 
-        # self.publisher_ = self.create_publisher(
-        #     DiagnosticArray,
-        #     '/diagnostics/rpi',
-        #     10
-        # )
+        self.publisher_ = self.create_publisher(
+            DiagnosticArray,
+            '/diagnostics/rpi',
+            10
+        )
         
-        self.cpu_temp_publisher = DiagnosticPublisher(self, DiagnosticParameter(
+        self.cpu_temp_status = StatusBuilder(self, StatusParameter(
             status_name="cpu_temp",
             hardware_id=self.hardware_id,
             normal_message="CPU temperature normal",
@@ -30,7 +30,7 @@ class RpiMonitorNode(Node):
             error_message="CPU overheating"
         ))
         
-        self.cpu_usage_publisher = DiagnosticPublisher(self, DiagnosticParameter(
+        self.cpu_usage_status = StatusBuilder(self, StatusParameter(
             status_name="cpu_usage",
             hardware_id=self.hardware_id,
             normal_message="CPU usage normal",
@@ -39,34 +39,55 @@ class RpiMonitorNode(Node):
             error_threshold=90,
             error_message="CPU usage critical"
         ))
+        
+        self.ram_usage_status = StatusBuilder(self, StatusParameter(
+            status_name="ram_usage",
+            hardware_id=self.hardware_id,
+            normal_message="RAM usage normal",
+            warning_threshold=80,
+            warning_message="RAM usage high",
+            error_threshold=90,
+            error_message="RAM usage critical"
+        ))
+        
+        self.disk_space_status = StatusBuilder(self, StatusParameter(
+            status_name="disk_space",
+            hardware_id=self.hardware_id,
+            normal_message="Disk space normal",
+            warning_threshold=80,
+            warning_message="Disk space high",
+            error_threshold=90,
+            error_message="Disk space critical"
+        ))
 
         self.timer = self.create_timer(1.0, self.timer_callback)
 
 
     def timer_callback(self):
         data = self.rpi_monitor.get_metric()
+
+        msg = DiagnosticArray()
+        msg.header.stamp = self.get_clock().now().to_msg()
+
+        msg.status.append(self.cpu_temp_status.build_status(data.cpu_temp))
+        msg.status.append(self.cpu_usage_status.build_status(data.cpu_usage))
+        msg.status.append(self.ram_usage_status.build_status(data.ram_usage_percent))
+        msg.status.append(self.disk_space_status.build_status(data.disk_usage_percent))
         
-        self.cpu_temp_publisher.publish(data.cpu_temp)
-        self.cpu_usage_publisher.publish(data.cpu_usage)
-
-        # msg = DiagnosticArray()
-        # msg.header.stamp = self.get_clock().now().to_msg()
-
-
-        # msg.status.append(status)
-
-        # self.publisher_.publish(msg)
+        self.publisher_.publish(msg)
         
-        
-
-
 
 def main(args=None):
     rclpy.init(args=args)
     node = RpiMonitorNode()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
--- src/resource_monitor/resource_monitor/status_builder.py
+++ src/resource_monitor/resource_monitor/status_builder.py
@@ -1,11 +1,13 @@
+from collections import deque
 from typing import Optional
 
 from rclpy.node import Node
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
 from dataclasses import dataclass
+import numpy as np
 
 @dataclass
-class DiagnosticParameter:
+class StatusParameter:
     status_name: str
     hardware_id: str
     normal_message: Optional[str]
@@ -14,33 +16,37 @@ class DiagnosticParameter:
     error_threshold: Optional[float]
     error_message: Optional[str]
     
-
-
-
-
-class DiagnosticPublisher:
-    def __init__(self, node: Node, diagnostic_parameter: DiagnosticParameter):
+@dataclass    
+class StatusStatistic:
+    mean: float
+    median: float
+    std: float
+    min: float
+    max: float
+    
+        
+class StatusBuilder:
+    def __init__(self, node: Node, diagnostic_parameter: StatusParameter, history_length: float = 1000):
         self.node = node
         self.diagnostic_parameter = diagnostic_parameter
         
-        self.publisher = node.create_publisher(
-            DiagnosticArray,
-            f'/diagnostics/{diagnostic_parameter.hardware_id}/{diagnostic_parameter.status_name}',
-            10
-        )
-    
-    def publish(self, data: float | str):
-        msg = DiagnosticArray()
-        msg.header.stamp = self.node.get_clock().now().to_msg()
+        self.history: list[float] = deque(maxlen=history_length)
 
+    
+    def build_status(self, data: float | str) -> DiagnosticStatus:
         status = DiagnosticStatus()
         status.name = self.diagnostic_parameter.status_name
         status.hardware_id = self.diagnostic_parameter.hardware_id
         
-        status = DiagnosticStatus()
-        
         if data is not None and type(data) == float:
+            statistics = self._calculate_statistics(data)
+            
             status.values.append(KeyValue(key=self.diagnostic_parameter.status_name, value=str(data)))
+            status.values.append(KeyValue(key='mean', value=str(statistics.mean)))
+            status.values.append(KeyValue(key='median', value=str(statistics.median)))
+            status.values.append(KeyValue(key='std', value=str(statistics.std)))
+            status.values.append(KeyValue(key='min', value=str(statistics.min)))
+            status.values.append(KeyValue(key='max', value=str(statistics.max)))
             
             if data < self.diagnostic_parameter.warning_threshold:
                 status.level = DiagnosticStatus.OK
@@ -51,10 +57,24 @@ class DiagnosticPublisher:
             else:
                 status.level = DiagnosticStatus.ERROR
                 status.message = self.diagnostic_parameter.error_message
-
-        msg.status.append(status)
-        self.publisher.publish(msg)
+                
 
         self.node.get_logger().info(
             f"{self.diagnostic_parameter.status_name} {data:.2f}" if data else f"{self.diagnostic_parameter.status_name}: unavailable"
         )
+        
+        return status
+    
+    def _calculate_statistics(self, data: float) -> StatusStatistic:
+        self.history.append(data)
+
+        values = np.asarray(self.history)
+
+        return StatusStatistic(
+            mean=round(float(np.mean(values)), 2),
+            median=round(float(np.median(values)), 2),
+            std=round(float(np.std(values)), 2),
+            min=round(float(np.min(values)), 2),
+            max=round(float(np.max(values)), 2),
+        )
+        
